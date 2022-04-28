9e91204bc21555ce6ee4943892c6a82fb9facfec
Raymond Lee <raymanl33@icloud.com>
2022-04-27 23:31:28 -0700

    add comments

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-var messages = document.querySelector("#messages");
-var textbox = document.querySelector("#text");
-var send = document.querySelector("#send");
+//chat box
+let messages = document.querySelector("#messages");
+let textbox = document.querySelector("#text");
+let send = document.querySelector("#send");
 
+
+//Firebase Set up =======================================
 const firebaseConfig = {
     apiKey: "AIzaSyCcqc8Se8jr5yA54_0SXAVs_YLQwtfLetU",
     authDomain: "messaging-app-d676c.firebaseapp.com",
@@ -12,18 +15,21 @@ const firebaseConfig = {
     measurementId: "G-57XJNKLQ2M"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const text = db.collection('messages');
 
+// When the send button gets clicked the message will be sent to firebase
 send.addEventListener("click", async(e) => {
     e.preventDefault();
 
-    var newMessage = document.createElement("li");
+    let newMessage = document.createElement("li");
     newMessage.innerHTML = textbox.value;
     messages.appendChild(newMessage);
     console.log(textbox.value)
 
+    
     await text.add({
         text: textbox.value,
         createdAt: firebase.firestore.FieldValue.serverTimestamp()
