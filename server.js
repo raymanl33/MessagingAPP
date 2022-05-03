a2ea78cb898766696ca29f1f5171c51ceac074dc
typout31 <nicholas.chu98@gmail.com>
2022-05-03 13:23:32 -0700

    Express Changes

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get("/goodbye", (req,res) => {
+    res.send("<h1>goodbye world!</h1>")
+})
+
+app.use(express.static("views"))
+
+const port = 8080
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
