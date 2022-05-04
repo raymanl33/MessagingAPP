1ee649ccad8c2509a2956e8e66417ca16d89ff03
typout31 <nicholas.chu98@gmail.com>
2022-05-03 19:13:18 -0700

    changes

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express()
+app.set("view engine", "ejs")
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+// app.get("/goodbye", (req,res) => {
+//     res.send("<h1>goodbye world!</h1>")
+// })
+
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.get("/goodbye", (req,res) => {
-    res.send("<h1>goodbye world!</h1>")
+  res.send(`<h1>${Date()}</h1>`)
 })
 
 app.use(express.static("views"))
